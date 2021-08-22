d7ca51248c3e939f72c0cad22c7cdc851c02fad4
guillaume <gbeldilmi@pm.me>
2021-08-23 00:20:50 +0200

    ft_strlen

--- libft.h
+++ libft.h
@@ -12,6 +12,7 @@ int ft_isdigit(int c);
 int ft_isprint(int c);
 int ft_isspace(char c);
 int ft_strcmp(const char *s1, const char *s2);
+size_t ft_strlen(const char *s);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 int ft_tolower(int c);
 int ft_toupper(int c);
