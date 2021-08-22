f00e4d37698be9d005de0184dab49379312fb869
guillaume <gbeldilmi@pm.me>
2021-08-23 00:22:06 +0200

    ft_strstr

--- libft.h
+++ libft.h
@@ -14,6 +14,7 @@ int ft_isspace(char c);
 int ft_strcmp(const char *s1, const char *s2);
 size_t ft_strlen(const char *s);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
+char *ft_strstr(char *str, char *to_find);
 int ft_tolower(int c);
 int ft_toupper(int c);
 
