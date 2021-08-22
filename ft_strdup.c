5c183d09cec6c8631e5be6fb2a3f1cd3f82c768b
guillaume <gbeldilmi@pm.me>
2021-08-23 00:26:41 +0200

    ft_strdup

---
+++ ft_strdup.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+char *ft_strdup(const char *s1)
+{
+  int i;
+  char *str;
+
+  str = NULL;
+  if (!(str = (char *) malloc(sizeof(char) * (ft_strlen(s) + 1))))
+  {
+    return (NULL);
+  }
+  i = 0;
+  while (s[i])
+  {
+    str[i] = s[i];
+    i++;
+  }
+  str[i] = '\0';
+  return (str);
+}
