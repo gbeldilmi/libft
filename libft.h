f506ca32f428d423c43c51c1b5a60133a2dab613
guillaume <gbeldilmi@pm.me>
2021-08-24 11:11:16 +0200

    ft_strnequ

--- libft.h
+++ libft.h
@@ -30,6 +30,7 @@ size_t ft_strlen(const char *s);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char *ft_strncat (char *dest, const char *src, size_t n);
 char *ft_strncpy(char *dest, const char *src, size_t n);
+int ft_strnequ(const char *s1, const char *s2, size_t n);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 char *ft_strnstr(const char *str, const char *to_find, size_t len);
 char *ft_strrchr(const char *s, int c);
