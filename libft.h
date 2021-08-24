d5b13c80ceebca37897c6953871a383faaf1338c
guillaume <gbeldilmi@pm.me>
2021-08-25 00:28:13 +0200

    ft_strdel

--- libft.h
+++ libft.h
@@ -25,6 +25,7 @@ char *ft_strchr(const char *s, int c);
 void ft_strclr(char *s);
 int ft_strcmp(const char *s1, const char *s2);
 char *ft_strcpy(char *dest, const char *src);
+void ft_strdel(char **as);
 char *ft_strdup(const char *s);
 int ft_strequ(const char *s1, const char *s2);
 char *ft_strjoin(const char *s1, const char *s2);
