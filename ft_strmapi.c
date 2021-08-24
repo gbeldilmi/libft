1b36e7b73532a37eb3c2ea590b28d2ff0fa08839
guillaume <gbeldilmi@pm.me>
2021-08-25 01:37:09 +0200

    ft_strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -9,16 +9,15 @@ char *ft_strmapi(const char *s, char (*f)(unsigned int, char))
   {
     return (NULL);
   }
-  if (!(str = (char *) malloc(sizeof(char) * (ft_strlen(s) + 1))))
+  if (str = (char *) malloc(sizeof(char) * (ft_strlen(s) + 1)))
   {
-    return (NULL);
+    i = 0;
+    while (s[i])
+    {
+      str[i] = f(i, s[i]);
+      i++;
+    }
+    str[i] = '\0';
   }
-  i = 0;
-  while (s[i])
-  {
-    str[i] = f(i, s[i]);
-    i++;
-  }
-  str[i] = '\0';
   return (str);
 }
