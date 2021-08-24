1798e137f71adf09e5b0953f98c567ab43deb561
guillaume <gbeldilmi@pm.me>
2021-08-25 01:26:53 +0200

    ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -7,18 +7,17 @@ char *ft_strjoin(const char *s1, const char *s2)
 
   i = 0;
   dest = (char *) malloc(sizeof(*dest) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-  if (dest == NULL)
+  if (dest)
   {
-    return (NULL);
+    while (*s1 != '\0')
+    {
+      dest[i++] = *s1++;
+    }
+    while (*s2 != '\0')
+    {
+      dest[i++] = *s2++;
+    }
+    dest[i] = '\0';
   }
-  while (*s1 != '\0')
-  {
-    dest[i++] = *s1++;
-  }
-  while (*s2 != '\0')
-  {
-    dest[i++] = *s2++;
-  }
-  dest[i] = '\0';
   return (dest);
 }
