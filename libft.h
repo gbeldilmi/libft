931101807467ed8386dd73b0f36aec46b4c030a1
guillaume <gbeldilmi@pm.me>
2021-08-24 10:56:39 +0200

    ft_strmapi

--- libft.h
+++ libft.h
@@ -26,6 +26,7 @@ char *ft_strcpy(char *dest, const char *src);
 char *ft_strdup(const char *s);
 size_t ft_strlcat(char *dst, const char *src, size_t size);
 size_t ft_strlen(const char *s);
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char *ft_strncat (char *dest, const char *src, size_t n);
 char *ft_strncpy(char *dest, const char *src, size_t n);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
