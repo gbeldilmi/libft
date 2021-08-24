d52f2b3166695a5968a0fc6698bb2f28bcf0c01b
guillaume <gbeldilmi@pm.me>
2021-08-25 00:56:41 +0200

    ft_strnew

--- libft.h
+++ libft.h
@@ -37,6 +37,7 @@ char *ft_strncat (char *dest, const char *src, size_t n);
 int ft_strncmp(const char *s1, const char *s2, size_t n);
 char *ft_strncpy(char *dest, const char *src, size_t n);
 int ft_strnequ(const char *s1, const char *s2, size_t n);
+char *ft_strnew(size_t size);
 char *ft_strnstr(const char *str, const char *to_find, size_t len);
 char *ft_strrchr(const char *s, int c);
 char *ft_strstr(char *str, char *to_find);
