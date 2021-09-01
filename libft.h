02678f8d6da65d31b43a730f4ba79a377a94c42a
guillaume <gbeldilmi@pm.me>
2021-09-01 17:18:35 +0200

    ft_lstadd

--- libft.h
+++ libft.h
@@ -22,6 +22,7 @@ int ft_isdigit(int c);
 int ft_isprint(int c);
 int ft_isspace(char c);
 char *ft_itoa(int n);
+void ft_lstadd(t_list **alst, t_list *new);
 void ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 t_list *ft_lstnew(const void *content, size_t content_size);
