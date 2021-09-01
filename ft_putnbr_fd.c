f4d2c1909eeec881feb540e125d9ef6eea3ee784
guillaume <gbeldilmi@pm.me>
2021-09-01 14:40:48 +0200

    ft_putnbr_fd

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+void ft_putnbr_fd(int n, int fd)
+{
+  if (n < 0)
+  {
+    ft_putchar_fd('-', fd);
+    n *= -1;
+  }
+  if (n < 10)
+  {
+    ft_putchar_fd(n + '0', fd);
+  }
+  else
+  {
+    ft_putnbr_fd(n / 10, fd);
+    ft_putnbr_fd(n % 10, fd);
+  }
+}
